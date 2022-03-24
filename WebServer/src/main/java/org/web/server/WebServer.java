f42484ca3940627525eb79d80a4851c831078304
Skylandia <xrealify@gmail.com>
2022-03-24 14:08:20 +1300

    Formatting

--- WebServer/src/main/java/org/web/server/WebServer.java
+++ WebServer/src/main/java/org/web/server/WebServer.java
@@ -9,28 +9,32 @@ import java.nio.file.Paths;
 import com.sun.net.httpserver.HttpServer;
 
 public class WebServer {
-	
+
 	/**
 	 * Creates a webserver on port 8080
+	 * 
 	 * @throws IOException
 	 */
 	public WebServer() throws IOException {
 		System.out.println("Starting HTTP server");
 		HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
 		File resourceDirectory = new File("src/main/resources");
+
 		for (File page : resourceDirectory.listFiles()) {
 			String relativePath = relativePath(resourceDirectory, page);
 			server.createContext("/" + relativePath, new PageHandler(relativePath));
 			System.out.println("Registered: /" + relativePath);
 		}
+
 		server.createContext("/", new PageHandler("index.html"));
 		server.setExecutor(null);
 		server.start();
 		System.out.println("Started HTTP server");
 	}
+
 	private static String relativePath(File source, File target) {
 		Path sourceFile = Paths.get(source.toURI());
-		Path targetFile = Paths.get(target.toURI()); 
+		Path targetFile = Paths.get(target.toURI());
 		Path relativePath = sourceFile.relativize(targetFile);
 		return relativePath.toString();
 	}

01fe6d526bf5dac328c503c0ed7778625c93f25a
Skylandia <xrealify@gmail.com>
2022-03-24 17:31:19 +1300

    Formatting and comments

--- WebServer/src/main/java/org/web/server/WebServer.java
+++ WebServer/src/main/java/org/web/server/WebServer.java
@@ -17,18 +17,24 @@ public class WebServer {
 	 */
 	public WebServer() throws IOException {
 		System.out.println("Starting HTTP server");
-		HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
-		File resourceDirectory = new File("src/main/resources");
 
+		HttpServer server = HttpServer.create(new InetSocketAddress(8080), 1);
+
+		// Add all files in the resource directory to the webserver
+		File resourceDirectory = new File("src/main/resources");
 		for (File page : resourceDirectory.listFiles()) {
 			String relativePath = relativePath(resourceDirectory, page);
 			server.createContext("/" + relativePath, new PageHandler(relativePath));
 			System.out.println("Registered: /" + relativePath);
 		}
 
+		// Reroute http://example.com/ to http://example.com/index.html
 		server.createContext("/", new PageHandler("index.html"));
-		server.setExecutor(null);
+
+		//server.setExecutor(null);
 		server.start();
+
+
 		System.out.println("Started HTTP server");
 	}
 

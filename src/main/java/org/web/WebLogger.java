bf62f59977683a2b534870096a1593395534a693
Skylandia <xrealify@gmail.com>
2023-04-02 00:06:26 +1300

    Update WebLogger

--- src/main/java/org/web/WebLogger.java
+++ src/main/java/org/web/WebLogger.java
@@ -1,14 +1,32 @@
 package org.web;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.Date;
 
 // Uses the singleton structure
 public class WebLogger {
 
-    private static final Logger webLogger = Logger.getLogger(WebLogger.class.getName());
+    private static int bufferCount = 0;
 
-    public static void log(Level level, String info) {
-        webLogger.log(level, info);
+    public static void log(String info) {
+        doBuffer();
+        System.out.println(new Date() + " | " + info);
+    }
+    public static void error(String info) {
+        doBuffer();
+        System.err.println(new Date() + " | " + info);
+    }
+
+    public static void buffer() {
+        buffer(1);
+    }
+    public static void buffer(int count) {
+        bufferCount += count;
+    }
+
+    private static void doBuffer() {
+        for (int i = 0; i < bufferCount; i++) {
+            System.out.println();
+        }
+        bufferCount = 0;
     }
 }

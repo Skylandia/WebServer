bbf70ad28ac5df5ed19eb6c1c663a1a8a3f2b054
Skylandia <xrealify@gmail.com>
2023-04-02 00:07:23 +1300

    Fix only top level resources being registered

--- src/main/java/org/web/server/WebServer.java
+++ src/main/java/org/web/server/WebServer.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.logging.Level;
 
 import com.sun.net.httpserver.HttpServer;
 
@@ -15,36 +14,59 @@ public class WebServer {
 
 	/**
 	 * Creates a webserver on port 80
-	 * 
+	 *
 	 * @throws IOException
 	 */
 	public WebServer() throws IOException {
-		WebLogger.log(Level.INFO, "Started HTTP server");
+		WebLogger.log("Started HTTP server");
+		WebLogger.buffer();
 
 		HttpServer server = HttpServer.create(new InetSocketAddress(80), 1);
 
 		// Add all files in the resource directory to the webserver
 		File resourceDirectory = new File("src/main/resources");
-		for (File page : resourceDirectory.listFiles()) {
-			String relativePath = relativePath(resourceDirectory, page);
-			server.createContext("/" + relativePath, new PageHandler(relativePath));
+		int resourceCount = addResources(server, resourceDirectory, resourceDirectory);
 
-			WebLogger.log(Level.INFO, String.format("Registered: /%s", relativePath));
-		}
+		WebLogger.log("Finished registering " + resourceCount + " resources");
+		WebLogger.buffer();
 
 		// Reroute http://example.com/ to http://example.com/index.html
-		server.createContext("/", new PageHandler("index.html"));
+		server.createContext("/", new PageHandler("/index.html"));
+
+		WebLogger.log("Rerouted root page \"/\" to \"/index.html\"");
+		WebLogger.buffer();
 
 		//server.setExecutor(null);
 		server.start();
 
-		WebLogger.log(Level.INFO, "HTTP server successfully started");
+		WebLogger.log("HTTP server successfully started");
+		WebLogger.buffer();
+	}
+
+	private static int addResources(HttpServer server, File resourceDirectory, File searchDirectory) throws IOException {
+		int resourceCount = 0;
+		for (File resource : searchDirectory.listFiles()) {
+			if (resource.isDirectory()) {
+				resourceCount += addResources(server, resourceDirectory, resource);
+				continue;
+			}
+
+			addResource(server, resourceDirectory, resource);
+			resourceCount++;
+		}
+		return resourceCount;
+	}
+
+	private static void addResource(HttpServer server, File resourceDirectory, File resource) throws IOException {
+		String relativePath = relativePath(resourceDirectory, resource);
+		server.createContext("/" + relativePath, new PageHandler(relativePath));
+		WebLogger.log(String.format("Registered: /%s", relativePath));
 	}
 
 	private static String relativePath(File source, File target) {
 		Path sourceFile = Paths.get(source.toURI());
 		Path targetFile = Paths.get(target.toURI());
 		Path relativePath = sourceFile.relativize(targetFile);
-		return relativePath.toString();
+		return relativePath.toString().replace('\\', '/');
 	}
 }

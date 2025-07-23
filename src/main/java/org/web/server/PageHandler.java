1673d03422707fcd9d59d4144df6eb9c0ff8dd82
Skylandia <52195906+Skylandia@users.noreply.github.com>
2025-07-24 11:33:53 +1200

    Handle MIME types

--- src/main/java/org/web/server/PageHandler.java
+++ src/main/java/org/web/server/PageHandler.java
@@ -1,29 +1,33 @@
 package org.web.server;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
 public class PageHandler implements HttpHandler {
-	
-	private byte[] pageBytes;
+
+	private final String contentType;
+	private final byte[] pageBytes;
 	
 	protected PageHandler(String pageName) throws IOException {
-		File page = new File("src/main/resources/" + pageName);
-		FileInputStream pageInputStream = new FileInputStream(page);
+		Path page = Path.of("src/main/resources", pageName);
+		FileInputStream pageInputStream = new FileInputStream(page.toFile());
 		pageBytes = pageInputStream.readAllBytes();
 		pageInputStream.close();
+		contentType = Files.probeContentType(page);
 	}
 	
 	@Override
 	public void handle(HttpExchange t) throws IOException {
+		t.getResponseHeaders().set("Content-Type", contentType);
 		t.sendResponseHeaders(200, pageBytes.length);
-		OutputStream os = t.getResponseBody();
-		os.write(pageBytes);
-		os.close();
+		try (OutputStream os = t.getResponseBody()) {
+			os.write(pageBytes);
+		}
 	}
 }
